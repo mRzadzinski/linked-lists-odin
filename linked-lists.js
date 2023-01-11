8accb77f5286d5ecda32f0e897db9c4cdf35eab5
Maciej Rzadzinski <maciej.rzd@gmail.com>
2023-01-11 20:58:42 +0100

    Create two classes: for list and node

---
+++ linked-lists.js
@@ -0,0 +1,12 @@
+class LinkedList {}
+
+class Node {
+    constructor() {
+        this.value = null;
+        this.nextNode = null;
+    }
+}
+
+let click = new LinkedList();
+click.value = 5;
+console.log(click)
